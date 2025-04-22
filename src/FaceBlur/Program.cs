8329071cee69a44a77bacfee99e51620bbb851d6
Janne Mattila <janne.mattila@hotmail.fi>
2025-04-22 12:19:10 +0300

    Update demo

--- src/FaceBlur/Program.cs
+++ src/FaceBlur/Program.cs
@@ -1,12 +1,11 @@
-﻿using ImageMagick;
-using Microsoft.Azure.CognitiveServices.Vision.Face;
-using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+﻿using Azure;
+using Azure.AI.Vision.Face;
+using ImageMagick;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FaceBlur
@@ -14,9 +13,9 @@ namespace FaceBlur
     public class Program
     {
         /// <summary>
-        /// Key from Face API resource (32 characters long).
+        /// Key from Face API resource.
         /// </summary>
-        private const string SubscriptionKey = "123456abcdef123456abcdef123456ab";
+        private const string SubscriptionKey = "long-key-here";
 
         /// <summary>
         /// Endpoint address from Face API resource. 
@@ -24,25 +23,24 @@ namespace FaceBlur
         /// Or in case of containers:
         /// http://localhost:5000
         /// </summary>
-        private const string FaceEndpoint = "http://localhost:5000";
+        private const string FaceEndpoint = "https://yourresourcename.cognitiveservices.azure.com";
 
         public static async Task Main(string[] args)
         {
-            var client = new FaceClient(new ApiKeyServiceClientCredentials(SubscriptionKey), new DelegatingHandler[] { })
-            {
-                Endpoint = FaceEndpoint
-            };
+            var client = new FaceClient(new Uri(FaceEndpoint), new AzureKeyCredential(SubscriptionKey));
 
             var fill = 1;
-            var input = @"C:\temp\demoimage.jpg";
-            var output = @"C:\temp\demoimage_output.jpg";
+            var input = @"C:\temp\source.jpg";
+            var output = @"C:\temp\target.jpg";
 
             try
             {
-                IList<DetectedFace> faces;
+                IReadOnlyList<FaceDetectionResult> faces;
                 using (var stream = File.OpenRead(input))
                 {
-                    faces = await client.Face.DetectWithStreamAsync(stream);
+                    BinaryData imageData = BinaryData.FromStream(stream);
+                    var response = await client.DetectAsync(imageData, FaceDetectionModel.Detection03, FaceRecognitionModel.Recognition04, false);
+                    faces = response.Value;
                 }
 
                 switch (fill)
@@ -63,43 +61,37 @@ namespace FaceBlur
             }
         }
 
-        private static void SolidFill(IList<DetectedFace> faces, string input, string output)
+        private static void SolidFill(IReadOnlyList<FaceDetectionResult> faces, string input, string output)
         {
             var brush = new SolidBrush(Color.DarkGray);
 
-            using (var bitmap = new Bitmap(input))
-            using (var stream = File.OpenWrite(output))
+            using var bitmap = new Bitmap(input);
+            using var stream = File.OpenWrite(output);
+            var graphics = Graphics.FromImage(bitmap);
+            foreach (var face in faces)
             {
-                var graphics = Graphics.FromImage(bitmap);
-                foreach (var face in faces)
-                {
-                    graphics.FillRectangle(brush,
-                        face.FaceRectangle.Left, face.FaceRectangle.Top,
-                        face.FaceRectangle.Width, face.FaceRectangle.Height);
-                }
-                graphics.Save();
-                bitmap.Save(stream, ImageFormat.Png);
+                graphics.FillRectangle(brush,
+                    face.FaceRectangle.Left, face.FaceRectangle.Top,
+                    face.FaceRectangle.Width, face.FaceRectangle.Height);
             }
+            graphics.Save();
+            bitmap.Save(stream, ImageFormat.Png);
         }
 
-        private static void BlurFill(IList<DetectedFace> faces, string input, string output)
+        private static void BlurFill(IReadOnlyList<FaceDetectionResult> faces, string input, string output)
         {
-            using (var image = new MagickImage(input))
+            using var image = new MagickImage(input);
+            foreach (var face in faces)
             {
-                foreach (var face in faces)
-                {
-                    using (var part = image.Clone())
-                    {
-                        part.Crop(new MagickGeometry(
-                            face.FaceRectangle.Left, face.FaceRectangle.Top,
-                            face.FaceRectangle.Width, face.FaceRectangle.Height));
-                        part.RePage();
-                        part.Blur(0, 5);
-                        image.Composite(part, face.FaceRectangle.Left, face.FaceRectangle.Top, CompositeOperator.Atop);
-                    }
-                }
-                image.Write(output, MagickFormat.Png);
+                using var part = image.Clone();
+                part.Crop(new MagickGeometry(
+                    face.FaceRectangle.Left, face.FaceRectangle.Top,
+                    (uint)face.FaceRectangle.Width, (uint)face.FaceRectangle.Height));
+                part.ResetPage();
+                part.Blur(0, 5);
+                image.Composite(part, face.FaceRectangle.Left, face.FaceRectangle.Top, CompositeOperator.Atop);
             }
+            image.Write(output, MagickFormat.Png);
         }
     }
 }
